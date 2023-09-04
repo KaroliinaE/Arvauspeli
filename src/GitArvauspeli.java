d54b252092270444f9654d611e780e8a2d9cb9a7
Karoliina <142309693+KaroliinaE@users.noreply.github.com>
2023-09-04 11:08:19 +0300

    Looppi oikealla nimellä

--- src/GitArvauspeli.java
+++ src/GitArvauspeli.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 
 public class GitArvauspeli {
 
 	public static void main(String[] args) {
 		// Arvauspeli GitHubiin
 		
-		
+		Scanner in = new Scanner(System.in);
 
+		String name;
+		
+		do
+		{
+			System.out.println("Arvaa minun nimeni.");
+			name=in.nextLine();
+			if (name.equals("Karoliina"))
+			{
+				break;
+			}
+		} while (!name.equals("Karoliina"));
+		
+		System.out.println("Onnittelut, arvasit oikein! Peli päättyy.");
+		
+		
 	}
 
 }
